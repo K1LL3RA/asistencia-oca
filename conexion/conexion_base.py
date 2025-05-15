43f9b798392b074d6e7ed1f760fb147b37291c8b
Omar Terrazas <143346101+K1LL3RA@users.noreply.github.com>
2025-05-15 18:01:54 -0500

    Update conexion_base.py

--- conexion/conexion_base.py
+++ conexion/conexion_base.py
@@ -1,17 +1,17 @@
 # conexion.py
 # Establece conexión con SQL Server usando pyodbc
-import pyodbc
+import pytds
 
 def obtener_conexion():
     try:
-        connection = pyodbc.connect(
-            'DRIVER={ODBC Driver 17 for SQL Server};'
-            'SERVER=98.84.221.29,1434;'
-            'DATABASE=SafeSmart_Checklist;'
-            'UID=oterrazas;'
-            'PWD=oca@2025'
+        conn = pytds.connect(
+            server='98.84.221.29,1434',
+            database='SafeSmart_Checklist',
+            user='jarbildo',
+            password='oca@2025',
+            port=1433  # Asegúrate de que tu SQL Server acepte conexiones remotas en este puerto
         )
-        return connection
-    except pyodbc.Error as e:
-        print(f"Error de conexión: {e}")
+        return conn
+    except Exception as e:
+        print("Error al conectar con pytds:", e)
         return None
