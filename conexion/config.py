d15428b4d1aae40239013650c52ffc0fe5a4a684
Omar Terrazas <143346101+K1LL3RA@users.noreply.github.com>
2025-05-15 18:08:33 -0500

    Update config.py

--- conexion/config.py
+++ conexion/config.py
@@ -2,10 +2,10 @@
 # Archivo de configuración centralizada para las variables del sistema
 # Datos de conexión a la base de datos SQL Server
 import os
-server = '98.84.221.29'
+server = '98.84.221.29,1434'
 database = 'SafeSmart_Checklist'
 username = 'jarbildo'
-password = 'oca$2025$'
+password = 'oca@2025'
 
 # Ruta fija a la plantilla de Excel base (puedes hacerla seleccionable desde la GUI si deseas)
 plantilla_path = os.path.join(os.path.dirname(__file__), "base.xlsx")#CAMBIARLO SEGUN SEA NESARIO
